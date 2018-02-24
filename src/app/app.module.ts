996362519016d2a31b97f452dd156b0dc34de982
Juan Ortiz <juanortiz@Juans-MacBook-Pro.local>
2018-02-24 12:42:02 -0500

    homepage created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,10 @@ import { NavComponent } from './components/nav/nav.component';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { FooterComponent } from './components/footer/footer.component';
+
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 
 
 @NgModule({
@@ -29,6 +33,7 @@ import { ModalModule } from 'ngx-bootstrap/modal';
     MainComponent,
     ForgotComponent,
     NavComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
@@ -41,6 +46,7 @@ import { ModalModule } from 'ngx-bootstrap/modal';
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
+    AngularFontAwesomeModule,
     routing
   ],
   providers: [
