bc024e075d4778f79756a878b90308f2fe5f0440
Sadiq Awosanmi <sadiqawos@gmail.com>
2018-02-25 22:04:04 -0500

    Adding a class to a DOM element when clicked

--- src/app/components/aboutus/aboutus.component.ts
+++ src/app/components/aboutus/aboutus.component.ts
@@ -6,16 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aboutus.component.css']
 })
 export class AboutusComponent implements OnInit {
-	expanded = false;
+  expandedQ1 = false;
+  expandedQ2 = false;
   constructor() { }
 
   ngOnInit() {
   }
 
-  toggleExpand() {
-  	console.log('this is working')
-  	this.expanded = !this.expanded;
+  toggleExpand(question) {
+    if (question === 'q1') {
+      this.expandedQ1 = !this.expandedQ1;
+    } else {
+      this.expandedQ2 = !this.expandedQ2;
+    }
   }
-
 }
-
