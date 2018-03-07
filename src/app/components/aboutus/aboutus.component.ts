d8dc7e6f4b10c422f3a7a96c515bcfe1e91e79e0
Juan Ortiz <juanortiz@Juans-MacBook-Pro.local>
2018-03-06 22:27:33 -0500

    finished up sessions page along with pricing  plus small updates to other pages

--- src/app/components/aboutus/aboutus.component.ts
+++ src/app/components/aboutus/aboutus.component.ts
@@ -8,16 +8,36 @@ import { Component, OnInit } from '@angular/core';
 export class AboutusComponent implements OnInit {
   expandedQ1 = false;
   expandedQ2 = false;
+  expandedQ3 = false;
+  expandedQ4 = false;
+  expandedQ5 = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+
   toggleExpand(question) {
     if (question === 'q1') {
       this.expandedQ1 = !this.expandedQ1;
-    } else {
+    } 
+    else if (question == 'q2') {
       this.expandedQ2 = !this.expandedQ2;
+    } 
+
+    else if (question == 'q3') {
+      this.expandedQ3 = !this.expandedQ3; 
     }
-  }
+
+    else if (question == 'q4') {
+      this.expandedQ4 = !this.expandedQ4; 
+    }
+
+    else if (question == 'q5') {
+      this.expandedQ5 = !this.expandedQ5; 
+    }
+            
+    }
+
 }
