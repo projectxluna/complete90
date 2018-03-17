b227478f8dd66b0a93831401733cba8cc4eb8c1c
Sadiq Awosanmi <sadiqawos@gmail.com>
2018-03-17 15:12:03 -0400

    show dynamic pricing

--- src/app/components/pricing/pricing.component.ts
+++ src/app/components/pricing/pricing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-pricing',
@@ -8,12 +9,38 @@ import { Component, OnInit } from '@angular/core';
 export class PricingComponent implements OnInit {
 
   isYearly = true;
-  constructor() { }
+  playerPrice = '240.00';
+  coachPrice = '300.00';
+  plans = {};
+
+  constructor(private dataService: DataService) {
+    this.dataService.getClient().subscribe((res) => {
+      if (res) {
+        this.processPlans(res.plans);
+      }
+    });
+  }
 
   ngOnInit() {
   }
 
-  toggleCycle() {
-    this.isYearly = !this.isYearly;
+  setCycle(cycle) {
+    if (cycle && cycle === 'yearly') {
+      this.coachPrice = this.plans['coach-yearly'].price || '300.0';
+      this.playerPrice = this.plans['player-yearly'].price || '240.0';
+
+      this.isYearly = true;
+    } else if (cycle && cycle === 'monthly') {
+      this.coachPrice = this.plans['coach-monthly'].price || '34.99';
+      this.playerPrice = this.plans['player-monthly'].price || '29.99';
+
+      this.isYearly = false;
+    }
+  }
+
+  processPlans(plans) {
+    plans.forEach(f => {
+      this.plans[f.id] = f;
+    });
   }
 }
