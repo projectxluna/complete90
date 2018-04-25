2c354e63df63f882205c6dc817b0ceae79c2df71
Juan Ortiz <juanortiz4@me.com>
2018-04-24 22:22:51 -0400

    linked registration forms to mailchimp, plus minor alignment fixes to content in events page

--- server/config.js
+++ server/config.js
@@ -6,20 +6,20 @@ var config = {
         database: 'mongodb://localhost:27017/complete90',
         braintree: {
             ENVIRONMENT: "Sandbox",
-            MERCHANT_ID: "",
-            PUBLIC_KEY: "",
-            PRIVATE_KEY: "",
+            MERCHANT_ID: "64wmhwjzmhx4zpsn",
+            PUBLIC_KEY: "db9j7d8kpm8j8ybj",
+            PRIVATE_KEY: "02015303a86d48d9a3badb0b8b5d81f8",
         },
         aws: {
-            PRIVATE_KEY: "",
-            PK_FILE: "",
-            CF_DOMAIN: "",
-            S3_BUCKET: "",
-            VIDEO_STRUCTURE: "",
+            PRIVATE_KEY: "APKAJMPYCKBXNVYFUIHA",
+            PK_FILE: "pk-APKAJMPYCKBXNVYFUIHA.pem",
+            CF_DOMAIN: "https://dqqdy81dl4r0o.cloudfront.net",
+            S3_BUCKET: "complete90",
+            VIDEO_STRUCTURE: "https://s3.us-east-2.amazonaws.com/complete90/config/video_structure.json",
         },
         mailer: {
-            EMAIL_ID: "",
-            PASSWORD: ""
+            EMAIL_ID: "projectxluna@gmail.com",
+            PASSWORD: "**********"
         },
         URL_EXPIRATION: 5,
     },
