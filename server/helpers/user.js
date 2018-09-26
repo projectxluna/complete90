717f6277aff7493538e437523e24fc8554e6dc57
Sadiq Awosanmi <sadiqawos@gmail.com>
2018-09-26 00:11:50 -0400

    return manager flag

--- server/helpers/user.js
+++ server/helpers/user.js
@@ -8,12 +8,14 @@
             let cleanUser = {
                 name: user.name,
                 email: user.email,
-                coach: user.coach,
+                coach: user.hasCoachSubscription(),
                 subscription: user.braintree.subscription,
                 creditCards: user.braintree.creditCards,
-                avatarURL: user.avatarURL || "/public/imgs/profile/cropped5ac0f4d48a2a273cd5f7b71a1526154727.jpg"
+                avatarURL: user.avatarURL || "/public/imgs/profile/cropped5ac0f4d48a2a273cd5f7b71a1526154727.jpg",
+                teamName: user.teamName,
+                companyname: user.companyname
             }
-            if (!user.coach) {
+            if (!user.hasCoachSubscription()) {
                 cleanUser.height = user.height;
                 cleanUser.position = user.position;
                 cleanUser.foot = user.foot;
