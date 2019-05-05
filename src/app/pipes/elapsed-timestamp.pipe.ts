cf9b9f161b78c0a6a0c6e4bfa9f6883f376132b6
Sadiq Awosanmi <sadiqawos@gmail.com>
2019-05-05 00:06:55 -0400

    fix leader board

--- src/app/pipes/elapsed-timestamp.pipe.ts
+++ src/app/pipes/elapsed-timestamp.pipe.ts
@@ -8,16 +8,17 @@ export class ElapsedTimestampPipe implements PipeTransform {
   transform(time: any, args?: any): any {
     var h = 0, m = 0, s = 0;
     var newTime = '';
-    let date = new Date(time);
 
-    h = date.getHours();
-    m = date.getMinutes();
-    s = date.getSeconds();
+    h = Math.floor(time / (60 * 60 * 1000));
+    time = time % (60 * 60 * 1000);
+    m = Math.floor(time / (60 * 1000));
+    time = time % (60 * 1000);
+    s = Math.floor(time / 1000);
 
     if (h > 0) {
       newTime += this.pad(h, 2) + ':';
     }
-    newTime = this.pad(m, 2) + ':' + this.pad(s, 2);
+    newTime += this.pad(m, 2) + ':' + this.pad(s, 2);
     return newTime;
   }
 
