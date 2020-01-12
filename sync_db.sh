5dc411cd84fea54c4401dc26b5df0039c7f32891
Sadiq Awosanmi <sadiqawos@gmail.com>
2020-01-12 18:17:11 -0500

    minor update to script

--- sync_db.sh
+++ sync_db.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
 
+DB_NAME=complete90
 # Dump data from production and restore into local db
 # This will also drop your local db
-ssh -i "~/ec2-sadiq.pem" ubuntu@ec2-18-222-149-10.us-east-2.compute.amazonaws.com "mongodump -d complete90 --archive=backup/tmp_dump.gz --gzip --archive" | mongorestore --drop complete90 -d complete90 --gzip --archive
+ssh -i "~/ec2-sadiq.pem" ubuntu@ec2-18-222-149-10.us-east-2.compute.amazonaws.com "mongodump -d $DB_NAME --archive=backup/tmp_dump.gz --gzip --archive" | mongorestore --drop $DB_NAME -d $DB_NAME --gzip --archive
 
 # Set all passwords to 'password1'
-mongo --eval 'db.user_profile.update({}, {$set: {password: "$2a$08$d6k/zM6nZYu0XJpRvlNnV.fkgdddaf6U2qB1wpBKjV7dwGaPSHSpa"}}, {multi: true})' complete90
+mongo --eval 'db.user_profile.update({}, {$set: {password: "$2a$08$d6k/zM6nZYu0XJpRvlNnV.fkgdddaf6U2qB1wpBKjV7dwGaPSHSpa"}}, {multi: true})' $DB_NAME
 exit
