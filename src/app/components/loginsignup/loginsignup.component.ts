354c741b621246dcfe188ffda49f8949e624adc9
vicknly <vick@nly.media>
2020-04-15 22:32:49 +0500

    Fixing login/signup

--- src/app/components/loginsignup/loginsignup.component.ts
+++ src/app/components/loginsignup/loginsignup.component.ts
@@ -37,7 +37,6 @@ export class LoginSignupComponent implements OnInit {
             this.dataService.getClient().subscribe((res) => {
                 if (res) {
                     this.processPlans(res.plans);
-                    this.setCycle('monthly');
                     console.log(this.plans);
                     this.disabled = false;
                 }
