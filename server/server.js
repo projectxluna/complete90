827a631868a41ffe9a4f610d5a4ad62e8424e178
vicknly <vick@nly.media>
2020-08-04 23:40:43 +0500

    Commented functions

--- server/server.js
+++ server/server.js
@@ -4,12 +4,14 @@ var bodyParser = require('body-parser');
 var fileUpload = require('express-fileupload');
 var morgan = require('morgan');
 var mongoose = require('mongoose');
-var mongodb = require('mongodb');
 var config = require('./config').get(process.env.NODE_ENV);
 var path = require('path');
+
 var PromoJob = require('./jobs/promo.job');
 var TopPlayerJob = require('./jobs/top_player.job');
 var PlayerAttributeJob = require('./jobs/player_attribute.job');
+// var LeaderboardJob = require('./jobs/leaderboard.job');
+
 var port = process.env.PORT || 9000;
 
 // mongoose config
@@ -54,4 +56,5 @@ app.get('*', (req, res) => {
 PromoJob.register();
 PlayerAttributeJob.register();
 TopPlayerJob.register();
+// LeaderboardJob.register();
 app.listen(port);
