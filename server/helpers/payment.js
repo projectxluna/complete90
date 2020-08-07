34fb107c4f1be4daca83cef17372ac6df391086d
vicknly <vick@nly.media>
2020-08-07 16:35:36 +0500

    Fixing subscription plan prices

--- server/helpers/payment.js
+++ server/helpers/payment.js
@@ -5,7 +5,7 @@ var btConfig = config.braintree;
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'dev') {
     btConfig.environment = braintree.Environment.Sandbox;
 } else {
-    btConfig.environment = braintree.Environment.Sandbox;
+    btConfig.environment = braintree.Environment.Production;
 }
 
 var gateway = braintree.connect(btConfig);
